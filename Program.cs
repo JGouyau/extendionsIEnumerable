139da2bb999219446dc53c0f65ab73b614399829
JGouyau <Gouyau.jimmy@gmail.com>
2021-09-28 21:44:45 +0200

    fini

--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace extendionsIEnumerable
     {
         static void Main(string[] args)
         {
+            int n = 32;
+            int[] tableauEntier = new int[] { 25, 2, 6, 1, 2, 5, 4, 12223200 };
+
+            double moyenneTableau ;
+            Console.WriteLine(" Tableau initial  " );
+            foreach (int num in tableauEntier)
+                Console.Write(" " + num  );
+            Console.WriteLine();
+
+            Console.WriteLine(" Le programme fait la moyenne des éléments d'un tableau d'entiers pour tout élément inférieur ou égal à un entier n.");
+            Console.WriteLine(" Choisissez la valeur de n");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("______");
+
+            Console.WriteLine("Nouveau tableau sans les nombre inférieur a " + n);
+
+            foreach (int num in tableauEntier.Where(num => num < n))
+                Console.Write(" " + num  );
+
+
+            moyenneTableau = tableauEntier.Where(num => num < n).Average();
+            Console.WriteLine();
+            Console.WriteLine("Moyenne des valeurs inférieur à " + n +" : "+ moyenneTableau);
+
+            Console.Read();
         }
     }
 }
